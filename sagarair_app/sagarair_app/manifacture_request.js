94debd02a2c41fe31829a02d517af4761fa456b8
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-13 10:38:58 +0530

    Delete sagarair_app/sagarair_app/manifacture_request.js

--- sagarair_app/sagarair_app/manifacture_request.js
+++
@@ -1,7 +0,0 @@
-frappe.ui.form.on('Sales Order', {
-    refresh: function(frm) {
-        frm.add_custom_button(__('Open MR'), function() {
-            frappe.set_route('List', 'Material Request', {'sales_order': frm.doc.name, 'status': 'Manufacture'});
-        });
-    }
-});
