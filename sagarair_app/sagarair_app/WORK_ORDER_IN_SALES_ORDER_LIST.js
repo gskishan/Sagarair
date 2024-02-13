8dc6cd735209f311d3a1c6016c2d97571b86f069
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-13 17:10:42 +0530

    Delete sagarair_app/sagarair_app/WORK_ORDER_IN_SALES_ORDER_LIST.js

--- sagarair_app/sagarair_app/WORK_ORDER_IN_SALES_ORDER_LIST.js
+++
@@ -1,7 +0,0 @@
-frappe.ui.form.on('Sales Order', {
-    refresh: function(frm) {
-        frm.add_custom_button(__('Open WO'), function() {
-            frappe.set_route('List', 'Work Order', {'sales_order': frm.doc.name});
-        });
-    }
-});
