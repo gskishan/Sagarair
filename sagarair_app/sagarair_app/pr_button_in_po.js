1e124859291c0f750b06745f3c29abda0cd8a5b7
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-13 10:39:23 +0530

    Delete sagarair_app/sagarair_app/pr_button_in_po.js

--- sagarair_app/sagarair_app/pr_button_in_po.js
+++
@@ -1,7 +0,0 @@
-frappe.ui.form.on('Purchase Order', {
-    refresh: function(frm) {
-        frm.add_custom_button(__('Open PR'), function() {
-            frappe.set_route('List', 'Purchase Receipt', {'purchase_order': frm.doc.name});
-        });
-    }
-});
