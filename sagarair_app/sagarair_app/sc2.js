4f88cca61729cc8159cc44130c9bc4613db5e391
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-13 17:10:51 +0530

    Delete sagarair_app/sagarair_app/sc2.js

--- sagarair_app/sagarair_app/sc2.js
+++
@@ -1,7 +0,0 @@
-frappe.ui.form.on("BOM", {
-    total_cost: function(frm,cdt,cdn) {
-        var d = locals[cdt][cdn];
-        frappe.model.set_value(cdt, cdn, 'total_cost_with_labour', (d.total_cost + d.labour_cost));
-    }
-});
-
