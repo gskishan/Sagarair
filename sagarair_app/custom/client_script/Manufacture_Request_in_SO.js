01f5d99e8223faced3525c83a7e429df99171212
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-14 10:24:20 +0530

    Update Manufacture_Request_in_SO.js

--- sagarair_app/custom/client_script/Manufacture_Request_in_SO.js
+++ sagarair_app/custom/client_script/Manufacture_Request_in_SO.js
@@ -3,5 +3,9 @@ frappe.ui.form.on('Sales Order', {
         frm.add_custom_button(__('Open MR'), function() {
             frappe.set_route('List', 'Material Request', {'sales_order': frm.doc.name, 'status': 'Manufacture'});
         });
+
+         frm.add_custom_button(__('Open WO'), function() {
+            frappe.set_route('List', 'Work Order', {'sales_order': frm.doc.name});
+        });
     }
 });
