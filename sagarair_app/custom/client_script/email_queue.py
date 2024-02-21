a27bfac938fd03253edc4e331efc0203d07a6c72
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-02-21 22:46:50 +0530

    Create email_queue.py

---
+++ sagarair_app/custom/client_script/email_queue.py
@@ -0,0 +1,7 @@
+import frappe
+@frappe.whitelist()
+def email_set():
+  if frappe.db.get_default("suspend_email_queue"):
+    frappe.db.set_default("suspend_email_queue", 0)
+  else:
+    frappe.db.set_default("suspend_email_queue", 1)
