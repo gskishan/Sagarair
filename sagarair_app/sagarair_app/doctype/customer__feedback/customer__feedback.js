e60d322bbd65b6e80acd8f13858262ce0ebe29b2
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-06-12 20:37:13 +0530

    Create customer__feedback.js

---
+++ sagarair_app/sagarair_app/doctype/customer__feedback/customer__feedback.js
@@ -0,0 +1,30 @@
+// Copyright (c) 2024, GS Kishan and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Customer  Feedback', {
+	refresh: function(frm) {
+	refresh(frm) {
+	if (cur_frm.doc.sales_order && cur_frm.is_new()) {
+    frappe.db.get_doc('Sales Order', cur_frm.doc.sales_order)
+        .then(doc => {
+
+            doc.items.forEach(function (item) {
+                var feedback_item = cur_frm.add_child("items");
+                feedback_item.item_code = item.item_code;
+                feedback_item.item_name = item.item_name;
+                feedback_item.qty = item.qty;
+                feedback_item.rate = item.rate;
+                feedback_item.description = item.description;
+                feedback_item.uom = item.uom;
+                 feedback_item.amount = item.amount;
+                feedback_item.delivery_date = item.delivery_date;
+
+
+            });
+            cur_frm.refresh_fields()
+
+        })
+}
+	}
+	}
+});
