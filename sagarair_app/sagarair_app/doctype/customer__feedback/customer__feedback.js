f4417ae003af04fbdcef1a1a8a8f267aca045b89
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-06-12 21:49:18 +0530

    Update customer__feedback.js

--- sagarair_app/sagarair_app/doctype/customer__feedback/customer__feedback.js
+++ sagarair_app/sagarair_app/doctype/customer__feedback/customer__feedback.js
@@ -2,29 +2,29 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Customer  Feedback', {
-	refresh: function(frm) {
-	if (cur_frm.doc.sales_order && cur_frm.is_new() && cur_frm.doc.items.length==0) {
-    frappe.db.get_doc('Sales Order', cur_frm.doc.sales_order)
-        .then(doc => {
+    refresh: function (frm) {
+        if (cur_frm.doc.sales_order && cur_frm.is_new() && !cur_frm.doc.items) {
+            frappe.db.get_doc('Sales Order', cur_frm.doc.sales_order)
+                .then(doc => {
 
-            doc.items.forEach(function (item) {
-                var feedback_item = cur_frm.add_child("items");
-                feedback_item.item_code = item.item_code;
-                feedback_item.item_name = item.item_name;
-                feedback_item.qty = item.qty;
-                feedback_item.rate = item.rate;
-                feedback_item.description = item.description;
-                feedback_item.uom = item.uom;
-                 feedback_item.amount = item.amount;
-                feedback_item.delivery_date = item.delivery_date;
-		feedback_item.conversion_factor = item.conversion_factor;
+                    doc.items.forEach(function (item) {
+                        var feedback_item = cur_frm.add_child("items");
+                        feedback_item.item_code = item.item_code;
+                        feedback_item.item_name = item.item_name;
+                        feedback_item.qty = item.qty;
+                        feedback_item.rate = item.rate;
+                        feedback_item.description = item.description;
+                        feedback_item.uom = item.uom;
+                        feedback_item.amount = item.amount;
+                        feedback_item.delivery_date = item.delivery_date;
+                        feedback_item.conversion_factor = item.conversion_factor;
 
 
-            });
-            cur_frm.refresh_fields()
+                    });
+                    cur_frm.refresh_fields()
 
-        })
+                })
 
-	}
-	}
+        }
+    }
 });
