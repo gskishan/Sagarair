9cd747f6a9ede71b3fa9a530d0b2ff663cc5d7e2
Swetha665279 <swetha@cruxedge.com>
2025-06-27 13:59:12 +0530

    Created a doctype named AHU Run Test Report

---
+++ sagarair_app/sagarair_app/doctype/ahu_run_test_report/test_ahu_run_test_report.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, GS Kishan and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAHURunTestReport(FrappeTestCase):
+	pass
